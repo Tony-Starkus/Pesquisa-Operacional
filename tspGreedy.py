05608dfdfccca2fccff56a7c4388b8702a1e8b5a
Evandro Cavalcante <evd99.jr@gmail.com>
2021-06-18 18:10:35 -0500

    Verificação de nós visitados

--- tspGreedy.py
+++ tspGreedy.py
@@ -9,17 +9,20 @@ def busca_gulosa(node_atual, distance_matriz: list, nodes):
     result = [node_atual]
     # distance_matriz_aux = distance_matriz
     while len(result) != len(distance_matriz):
-        print(sorted(distance_matriz[node_atual]))
-        exit(1)
+        #print(sorted(distance_matriz[node_atual]))
+        #exit(1)
+        # verificação para não adicionar nós já visitados no result
         for i in range(1, len(distance_matriz)):
             if i != 0 and distance_matriz[node_atual].index(sorted(distance_matriz[node_atual])[i]) not in result:
                 aux = sorted(distance_matriz[node_atual])[i]
+                vizinho = distance_matriz[node_atual].index(sorted(distance_matriz[node_atual])[i])
+                break;
 
-        for i in range(len(distance_matriz)):
+        """for i in range(len(distance_matriz)):
             if i not in result:
                 menor_dist = sorted(distance_matriz[node_atual])[i]
-                vizinho = distance_matriz[node_atual].index(menor_dist)
-
+                vizinho = distance_matriz[node_atual].index(menor_dist)"""
+        #print(aux)
         # print(distance_matriz[node_atual])
         # print(nodes[vizinho])
         # print(menor_dist, vizinho)
